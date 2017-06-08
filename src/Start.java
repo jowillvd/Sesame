1153a9c4c1b073ce26a6f065703ed672ef1e57ed
williamvdijk <jordyvdijk@gmail.com>
2017-06-08 13:23:35 +0200

    First commit

--- src/Start.java
+++ src/Start.java
@@ -1,9 +1,30 @@
+import controller.KluisController;
+import controller.SchatkamerController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import model.Geestkaart;
+import model.Kluis;
+import model.Schatkamer;
 
-public class Start {
+public class Start extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//launch(args);
+		Geestkaart slot = new Geestkaart();
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		/*Kluis kluis = new Kluis();
+		Schatkamer schatkamer = new Schatkamer();
+		KluisController kluisController = new KluisController();
+		SchatkamerController schatkamerController = new SchatkamerController();
+
+		if(kluis.isOpen()) {
+
+		} else {
+
+		}*/
 	}
 
 }
