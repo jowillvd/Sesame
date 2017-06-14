7edca62f18cf8fd0e95a7eaa8e3e081b080fc3f3
s1102308 <s1102308@student.hsleiden.nl>
2017-06-14 10:46:08 +0200

    Create Kaart.java

--- src/model/kaarten/Kaart.java
+++ src/model/kaarten/Kaart.java
@@ -2,10 +2,13 @@ package model.kaarten;
 
 public interface Kaart {
 
-	public boolean gepakt = false;
 
-	public boolean isGepakt();
-	public boolean setGepakt();
-	public void toonKaart();
+	public void setGepakt(boolean pak){;
+	}
+	
+	public boolean isGepakt(boolean gepakt){
+	}
+	
+	public void toonKaart(String icoon);
 
 }
