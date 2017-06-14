af57b6519ab7cd1ee60a2ee187c144e4e87edd2d
s1102308 <s1102308@student.hsleiden.nl>
2017-06-14 10:59:22 +0200

    Update Kaart.java

--- src/model/kaarten/Kaart.java
+++ src/model/kaarten/Kaart.java
@@ -1,14 +1,10 @@
 package model.kaarten;
 
 public interface Kaart {
+	public void setGepakt(boolean gepakt);
 
+	public boolean isGepakt();
 
-	public void setGepakt(boolean pak){;
-	}
-	
-	public boolean isGepakt(boolean gepakt){
-	}
-	
 	public void toonKaart(String icoon);
 
-}
+	}
