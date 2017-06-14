7b4662f03d4c29ae0fc75b63b842e4ef980bb619
s1102308 <s1102308@student.hsleiden.nl>
2017-06-14 11:05:49 +0200

    Update Slang.java

--- src/model/kaarten/Slang.java
+++ src/model/kaarten/Slang.java
@@ -1,5 +1,24 @@
 package model.kaarten;
 
-public class Slang {
+public class Slang implements Kaart{
+	private boolean gepakt;
+
+	@Override
+	public void setGepakt(boolean gepakt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isGepakt() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void toonKaart(String icoon) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
