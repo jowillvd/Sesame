c01f7636d45f79192774a5e9d5c50d4a579d781f
s1102308 <s1102308@student.hsleiden.nl>
2017-06-14 11:04:02 +0200

    Update Kelk.java

--- src/model/kaarten/Kelk.java
+++ src/model/kaarten/Kelk.java
@@ -1,5 +1,45 @@
 package model.kaarten;
 
-public class Kelk {
+public class Kelk implements Schat{
+	private int waarde;
+	private boolean gepakt;
+
+	@Override
+	public void setGepakt(boolean gepakt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isGepakt() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void toonKaart(String icoon) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getWaarde() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setSpeler(String speler) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getSpeler() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 
 }
