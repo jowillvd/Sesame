a24acae120f02b3fab4a8cbeb6453ff121a558a8
s1102308 <s1102308@student.hsleiden.nl>
2017-06-14 11:24:49 +0200

    Update KaartHouder.java

--- src/model/KaartHouder.java
+++ src/model/KaartHouder.java
@@ -1,5 +1,17 @@
 package model;
 
 public class KaartHouder {
+	private KaartHouderFactory kaartHouders;
+
+	KaartHouder kaarthouder = new KaartHouder();
+
+	public void isLeeg(){
+
+	}
+
+	public KaartHouderFactory getKaartHouders(){
+		return this.kaartHouders;
+
+	}
 
 }
