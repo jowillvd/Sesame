4a5578eaa5f09bf89425c487f5cdced04215111a
s1102308 <s1102308@student.hsleiden.nl>
2017-06-14 11:05:11 +0200

    Update Ring.java

--- src/model/kaarten/Ring.java
+++ src/model/kaarten/Ring.java
@@ -1,5 +1,44 @@
 package model.kaarten;
 
-public class Ring {
+public class Ring implements Schat{
+	private int waarde;
+	private boolean gepakt;
+
+	@Override
+	public void setGepakt(boolean gepakt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isGepakt() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void toonKaart(String icoon) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getWaarde() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setSpeler(String speler) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getSpeler() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
