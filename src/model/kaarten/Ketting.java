1a0ab34adaab98f5a4a45aea0ac0428daece3ead
s1102308 <s1102308@student.hsleiden.nl>
2017-06-14 11:41:28 +0200

    Update Ketting.java

--- src/model/kaarten/Ketting.java
+++ src/model/kaarten/Ketting.java
@@ -12,8 +12,7 @@ public class Ketting implements Schat{
 
 	@Override
 	public boolean isGepakt() {
-		// TODO Auto-generated method stub
-		return false;
+		return gepakt;
 	}
 
 	@Override
@@ -24,8 +23,7 @@ public class Ketting implements Schat{
 
 	@Override
 	public int getWaarde() {
-		// TODO Auto-generated method stub
-		return 0;
+		return waarde;
 	}
 
 	@Override
